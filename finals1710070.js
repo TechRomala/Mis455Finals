c6076617db72a7542bc7ae98415445b6d7bfc43c
TechRomala <1710070@iub.edu.bd>
2022-12-16 10:55:04 +0600

    first commit

---
+++ finals1710070.js
@@ -0,0 +1,45 @@
+function connect() {
+
+
+    var searchText = document.getElementById('Search').value; 
+    var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+    var mealNumber = document.getElementById('mealNumber').value;
+
+
+    fetch(url)
+    .then(res => res.json() )
+    .then(data => showData(data));
+   
+    document.getElementById('Search').value = ""; 
+    document.getElementById('mealNumber').value= "";
+}
+   
+function showData(data){
+   
+    console.log("data from showData Function", data);
+   
+    var oldContent = document.getElementById('container');
+    oldContent.textContent= "";
+   
+    for (var i=0; i< data.meals.length; i++){
+        console.log(data.meals[i]);
+   
+        var newDiv = document.createElement('div');
+        newDiv.innerHTML = `<div class="card" style="width: 18rem;">
+        <img src="${data.meals[i].strMealThumb}" class="card-img-top img-fluid" alt="...">
+        <div class="card-body">
+            <h5 class="card-title" id="itemName">${data.meals[i].strMeal}</h5>
+            <p class="card-text">Fulfill your appetite now</p>
+            <a href="#" class="btn btn-secondary" id="addCart">Order Now</a>
+           </div>
+       </div>`
+       
+        newDiv.classList.add("meal-style");
+           
+        oldContent.appendChild(newDiv);
+        
+   
+          }
+      
+   
+   }
